19fb9a2ca7549519f2fbd6aab321703ff7ead3b0
Lee Clark <wymux@shoash.com>
2024-03-16 00:17:21 +0000

    Complete Part 5

---
+++ src/components/Middle/MiddleComponent.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import NewPost from "./NewPost";
+import PostRender from "./PostRender";
+import QueryWrapper from "../QueryWrapper";
+
+type Props = {};
+
+const MIddleComponent = (props: Props) => {
+  return (
+    <div>
+      <NewPost />
+      <QueryWrapper>
+        <PostRender />
+      </QueryWrapper>
+    </div>
+  );
+};
+
+export default MIddleComponent;
